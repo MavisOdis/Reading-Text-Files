d8019fb8168ff91aa6ac75941d9120c1e26ce4c1
Odivwofene Mavis <odivwofene@gmail.com>
2022-05-27 08:49:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,27 +3,32 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content():
-    # [assignment] Add your code here 
-    with open("story.txt" , 'r') as f:
-        var = f.read()
-        print(var)
-        f.close()
 
 
+def read_file_content(filename):
+     
+    with open("story.txt" , 'r') as filename:
+
+        story = filename.read()
+        
+        return story
+        
 
 def count_words():
-    text = read_file_content("story.txt")
-    # [assignment] Add your code here
-    count = 0
-    for word in text:
-        count += 1
-        return {"as": 10, "would": 20}
+    text = read_file_content("./story.txt")
+    words = text.split()
+    occurance = dict()
+ 
+    for i in words:
 
-# print(count_words)
-# read_file_content()
+        if i in occurance:
+            occurance[i] += 1
 
-with open("read.txt", 'r') as f:
-    var = f.read()
-    print(var)
-    f.close()
+        else:
+            occurance[i] = 1
+
+    return occurance        
+
+        
+
+print(count_words())
